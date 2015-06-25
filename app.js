78477982a38ef3d65f6bcf918793848382e9641c
Brian Byers <bbyers@pivotal.io>
2015-06-24 20:59:23 -0500

    added refresh back and local cache

--- app.js
+++ app.js
@@ -1,34 +1,14 @@
 var Hapi = require('hapi');
 var Promise = require('bluebird');
-var redis = Promise.promisifyAll(require("redis"));
+var _ = require('lodash');
+var request = require('request');
 
 var server = new Hapi.Server();
 server.connection({ port: process.env.PORT || 3000 });
 
-if (process.env.VCAP_SERVICES) {
-	var services = JSON.parse(process.env.VCAP_SERVICES);
-	var credentials = services.rediscloud[0].credentials;
-} else {
-	credentials = {
-		port: 6379,
-		hostname: 'localhost',
-		password: null
-	}
-}
-var client = redis.createClient(
-	credentials.port,
-	credentials.hostname,
-	{
-		auth_pass: credentials.password
-	}
-);
-
-client = Promise.promisifyAll(client);
-client.on("error", function (err) {
-    console.log("Redis Error " + err);
-});
-
 var url = 'http://www.hidemyass.com/proxy-list/';
+
+var proxyList, latestProxy;
 
 server.route({
     method: 'GET',
@@ -54,110 +34,236 @@ server.route({
     handler: latestProxy
 });
 
+server.route({
+    method: 'GET',
+    path: '/refresh',
+    handler: function(request, reply) {
+        refreshProxies();
+        reply('refreshing');
+    }
+});
+
+function resetProxyList() {
+    proxyList = {
+        all: [],
+        http: [],
+        https: []
+    };
+
+    latestProxy = {
+        any: {},
+        http: {},
+        https: {}
+    };
+}
+
+function refreshProxies() {
+    console.log('app.js: refreshProxies()');
+    resetProxyList();
+    getProxies(1);
+    getProxies(2);
+    getProxies(3);
+    getProxies(4);
+    getProxies(5);
+    
+}
+
+function addProxy(proxy) {
+    proxyList.all.push(proxy);
+
+    if (proxy.type === 'HTTP') {
+        proxyList.http.push(proxy);
+    }
+
+    if (proxy.type === 'HTTPS') {
+        proxyList.https.push(proxy);
+    }
+    
+}
+
+function addLatestProxy(proxy) {
+    latestProxy.any =proxy;
+
+    if (proxy.type === 'HTTP') {
+        latestProxy.http = proxy;
+    }
+
+    if (proxy.type === 'HTTPS') {
+        latestProxy.https = proxy;
+    }
+}
 function listProxies (request, reply) {
-    var p = proxyListExists().then(function(exists) {
-    	return exists;
-    }).then(function(exists) {
-    	if (exists) {
-    		if (!request.params.protocol) {
-    			return getProxyList();
-    		} else if (request.params.protocol === 'http') {
-    			return getHttpProxyList();
-    		} else if (request.params.protocol === 'https') {
-    			return getHttpsProxyList();
-    		}
-    	}
-		return [];
-    }).map(function(proxy) {
-		return JSON.parse(proxy);
-	}).then(function(proxyList) {
-		reply(proxyList);
-	});
+    if (!proxyListExists()) {
+        reply([]);
+    }   
+
+    var proxyies = []
+    if (!request.params.protocol) {
+        proxyies = getProxyList();
+    } else if (request.params.protocol === 'http') {
+        proxies = getHttpProxyList();
+    } else if (request.params.protocol === 'https') {
+        proxies = getHttpsProxyList();
+    }
+
+    reply(proxyies);
 }
 
 
 function randomProxy (request, reply) {
-    var p = proxyListExists().then(function(exists) {
-    	return exists;
-    }).then(function(exists) {
-    	if (exists) {
-    		if (!request.params.protocol) {
-    			return getRandomProxy();
-    		} else if (request.params.protocol === 'http') {
-				return getRandomHttpProxy();
-    		} else if (request.params.protocol === 'https') {
-				return getRandomHttpsProxy();
-    		}
-			
-    	} else {
-    		p.cancel();
-    		reply({});
-    	}
-    }).then(function(proxy) {
-		reply(JSON.parse(proxy));
-    });
+    if (!proxyListExists()) {
+        reply({});
+    }
+
+    var proxy = {};
+    if (!request.params.protocol) {
+        proxy = getRandomProxy();
+    } else if (request.params.protocol === 'http') {
+        proxy = getRandomHttpProxy();
+    } else if (request.params.protocol === 'https') {
+        proxy = getRandomHttpsProxy();
+    }
+
+	reply(proxy);
 }
 
 function latestProxy (request, reply) {
-    var p = proxyListExists().then(function(exists) {
-    	return exists;
-    }).then(function(exists) {
-    	if (exists) {
-			if (!request.params.protocol) {
-				return getLatestProxy();
-			} else if (request.params.protocol === 'http') {
-				return getLatestHttpProxy();
-			} else if (request.params.protocol === 'https') {	
-				return getLatestHttpsProxy();
-			}
-    	} else {
-    		p.cancel();
-    		reply({});
-    	}
-    }).then(function(proxy) {
-    	reply(JSON.parse(proxy));
-	});
+    if (!proxyListExists()) {
+        reply({});
+    }
+
+    var proxy = {};
+	if (!request.params.protocol) {
+		proxy = getLatestProxy();
+	} else if (request.params.protocol === 'http') {
+		proxy = getLatestHttpProxy();
+	} else if (request.params.protocol === 'https') {	
+		proxy = getLatestHttpsProxy();
+	}
+
+	reply(proxy);
 }
 
 function proxyListExists() {
-	return client.existsAsync('proxy-list');
+	return proxyList.all.length != 0;
 }
 
 function getRandomProxy() {
-	return client.srandmemberAsync('proxy-list');
+	return _.sample(proxyList.all, 1);
 }
 
 function getRandomHttpProxy() {
-	return client.srandmemberAsync('proxy-list-http');
+    return _.sample(proxyList.http, 1);
 }
 
 function getRandomHttpsProxy() {
-	return client.srandmemberAsync('proxy-list-https');
+	return _.sample(proxyList.https, 1);
 }
 
 function getLatestProxy() {
-	return client.getAsync('proxy-latest');
+	return latestProxy.any;
 }
 
 function getLatestHttpProxy() {
-	return client.getAsync('proxy-latest-http');
+	return latestProxy.http;
 }
 
 function getLatestHttpsProxy() {
-	return client.getAsync('proxy-latest-https');
+	return latestProxy.https;
 }
 
 function getProxyList() {
-	return client.smembersAsync('proxy-list');
+	return proxyList.all;
 }
 
 function getHttpProxyList() {
-	return client.smembersAsync('proxy-list-http');
+	return proxyList.http;
 }
 
 function getHttpsProxyList() {
-	return client.smembersAsync('proxy-list-https');
+	return proxyList.https;
 }
+
+
+function getProxies(page){
+    fakeNums = {};
+    request(url + page, function(err, res, body){
+        if (err || !res || res.statusCode !== 200) {
+            console.log("Response code was not 200");
+            return;
+        }
+
+        var ips = [];
+        var ports = [];
+        var types = [];
+
+        body.replace(/\.(.*?)\{display\:none\}/g, function () {
+            //arguments[0] is the entire match
+            fakeNums[arguments[1]] = 1;
+        });
+
+        body.replace(/<td>(.*?)<\/td>/g, function () {
+            if(arguments[1] === "HTTP" || arguments[1] === "HTTPS" || arguments[1] === "socks4/5") {
+                types.push(arguments[1]);
+            }
+        });
+
+        var trim = body;
+        trim = trim.replace(/\s/g,'');
+
+        trim.replace(/<td>([0-9]+)<\/td>/g, function () {
+            ports.push(arguments[1]);
+        });
+
+        body.replace(/<\/style>(.*?)<\/td>/g, function () {
+            var temp = arguments[1];
+            temp = temp.replace(/<span class\=\"(.*?)\">.*?<\/span>/g,function(){
+                if(fakeNums[arguments[1]]){
+                    return '';
+                }
+                return arguments[0];
+            })
+            temp = temp.replace(/<span style\=\"display\:none\">(.*?)<\/span>/g,"");
+            temp = temp.replace(/<div style\=\"display\:none\">(.*?)<\/div>/g,"");
+            temp = temp.replace(/<(.*?)>/g,'');
+            ips.push(temp);
+        })
+
+        var count = 0;
+        var max = ips.length;
+
+        if(max > 0){
+            if(ports.length == 0 || ports.length != ips.length || ips.length != types.length) {
+                console.log("Regex parsing has failed.");
+                return;
+            }
+            
+            var foundHttp = false;
+            var foundHttps = false;
+            for(var i = 0; i < max; i++){
+                if(types[i] === 'HTTP' || types[i] === 'HTTPS'){
+                    count++
+                    var address, port, type;
+                    if (page === 1) { //only look at the first page, has the latest
+                        if (types[i] === 'HTTP' && foundHttp === false) { //look for the first HTTP proxy
+                            foundHttp = true;
+                            addLatestProxy({address:ips[i], port:ports[i], type:types[i]});
+                        } else if (types[i] === 'HTTPS' && foundHttps === false) { //look for the first HTTPS proxy
+                            foundHttps = true;
+                            addLatestProxy({address:ips[i], port:ports[i], type:types[i]}); 
+                        }
+                    }
+                    addProxy({address:ips[i], port:ports[i], type:types[i]}); //add the proxy to the main list
+                }
+            }
+        }   
+    });
+}
+
+refreshProxies();
+setInterval(function() {
+    refreshProxies();
+}, 600000); //every 10min
 
 server.start(function () {
     console.log('Server running at:', server.info.uri);
